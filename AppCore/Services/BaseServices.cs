822448cda95139662ee18b40d518d7fd7ed78dcf
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2021-12-02 09:32:51 -0600

    Add files via upload

---
+++ AppCore/Services/BaseServices.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Services
+{
+    public class BaseServices<T> : IServices<T>
+    {
+        private IModel<T> Model;
+        protected BaseServices(IModel<T> model)
+        {
+            this.Model = model;
+        }
+        public void Create(T t)
+        {
+            Model.Create(t);
+        }
+
+        public void Delete(T t)
+        {
+            Model.Delete(t);
+        }
+
+        public int GetLastId()
+        {
+            return Model.GetLastId();
+        }
+
+        public ICollection<T> Read()
+        {
+            return Model.Read();
+        }
+    }
+}
---
+++ AppCore/Services/NotaServices.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Services
+{
+    public class NotaServices : BaseServices<Notas>, NotaRepositoryService
+    {
+        // Aqui van todos los metodos de EstudianteRepositoryServices.
+        private INotasRepository Repository;
+        public NotaServices(INotasRepository repository) : base(repository)
+        {
+            this.Repository = repository;
+        }
+
+        public int CalcularNotas(int primer, int segundo)
+        {
+            return Repository.CalcularNotas(primer, segundo);
+        }
+
+        public int CalcularPromedio()
+        {
+            return Repository.CalcularPromedio();
+        }
+
+        public Estudiante EstudianteById(int Id)
+        {
+            return Repository.EstudianteById(Id);
+        }
+
+        public ICollection<Asignaturas> GetAsignaturas()
+        {
+            return Repository.GetAsignaturas();
+        }
+
+        public ICollection<Notas> MejoresEstudiantes()
+        {
+            return Repository.MejoresEstudiantes();
+        }
+    }
+}
