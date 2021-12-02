849b6c939ea448b84f9edc5ae1ff63845ef60fa6
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2021-12-02 09:30:16 -0600

    agregando todo

---
+++ AppCore/Interfaces/NotaRepositoryService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Services
+{
+    public interface NotaRepositoryService : IServices<Notas>
+    {
+        Estudiante EstudianteById(int Id);
+        int CalcularPromedio();
+        ICollection<Notas> MejoresEstudiantes();
+        ICollection<Asignaturas> GetAsignaturas();
+        int CalcularNotas(int primer, int segundo);
+    }
+}
